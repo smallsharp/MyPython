3851ef01d1aeb7c3a3d16306dc9e059ce6ade19e
PC-201803031124\Administrator <464625206@qq.com>
2018-03-06 00:00:16 +0800

    面向对象使用更新

--- basic/面向对象/property的使用方式1.py
+++ basic/面向对象/property的使用方式1.py
@@ -1,4 +1,5 @@
 class Person(object):
+
     def __init__(self):
         self.__age = 18
 
@@ -10,13 +11,14 @@ class Person(object):
         print("set")
         self.__age = value
 
+    # 使用property关键字
     age = property(get_age, set_age)
 
+
 p = Person()
-print(p.age)
-print(p.__dict__) # {'_Person__age': 18}
+print(p.age) # 调用get_age() 方法
+# print(p.__dict__)  # {'_Person__age': 18}
 
-p.age = 20 # 修改属性值，调用set_agesssss
-print(p.__dict__) # {'_Person__age': 20}
+p.age = 20  # 修改属性值，调用set_age,并没有新增属性
+# print(p.__dict__)  # {'_Person__age': 20}
 print(p.age)
-
