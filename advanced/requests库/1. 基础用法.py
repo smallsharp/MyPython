1d2120f9e542e21ba739d887af7152a367ce3f7c
likai <464625206@qq.com>
2018-04-10 19:28:57 +0800

    requests

--- advanced/requests库/1. 基础用法.py
+++ advanced/requests库/1. 基础用法.py
@@ -1,44 +1,49 @@
 # coding=utf-8
 import requests
 
+# 1.请求的url
 url = "https://m.taidu.com/goodsSite/home/categoryProductList"
+# url = "https://m.taidu.com/goodsSite/home/categoryProductList?abbr=CN&clientType=H5&clientVersion="
 
-# 传递参数
-params = {"abbr": "CN", "clientType": "H5", "clientVersion": None}
+# 2.请求参数
+params = {"abbr": "CN", "clientType": "H5","clientVersion":""}
 
-# 添加header
-headers = {'content-type': 'application/json'}
+# 3.请求header
+headers = {'Content-Type': 'application/x-www-form-urlencoded'}
 
-res = requests.get(url=url, params=params, headers=headers)
+# 4.发送请求
+res = requests.get(url,params,headers = headers)
 
-"""
-响应内容
-"""
-# 响应的状态码
+
+# 5.响应的状态码
 print("status_code：",res.status_code)
 
-# 文本
+# 6.响应的文本
 print("response text：",res.text)
 
-# 字节的方式访问请求响应体
+# 7.响应的文本（字节）
 print("response content:",res.content)
 
 with open(file="res_content.json",mode="wb") as f:
     f.write(res.content)
 
-# 返回结果编码
+# 8.响应的结果编码
 print("response encoding:",res.encoding)
 
 # 转成json格式
-print("response json:",res.json())
+# print("response json:",res.json())
 
+# 9.响应的头信息
 print("response headers:",res.headers)
 
+# 10.响应的cookies
 print("response cookies:",res.cookies)
 
 for k,v in res.cookies.items():
     print("cookies:{}={}".format(k,v))
 
+# 11.响应的url
 print("response url:",res.url)
 
+# 12.响应的请求历史
 print("response history:",res.history)
