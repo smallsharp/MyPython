c6bfcbcd491bae0c02c8910befd7c6bf8301723f
LAPTOP-GBRN5FU6\likai <464625206@qq.com>
2018-10-16 19:02:33 +0800

    正则

---
+++ advanced/requests库/9. cookies的处理.py
@@ -0,0 +1,50 @@
+import requests
+
+
+cookies = None
+
+
+def updateCookies():
+    '''
+    通过登录的方式获取cookies
+    :return:cookies
+    '''
+    global cookies
+    url = 'https://apibeta.yunchehome.com/user/login'
+    data = {"mobile": "18516213133", "password": "123456", "captcha": ""}
+    res = requests.post(url, json=data)
+    cookies = res.cookies
+    return cookies
+
+
+def getCookies():
+    '''
+    防止每次请求都获取cookies，有了就不重复获取
+    :return:
+    '''
+    if not cookies:
+        print('update')
+        return updateCookies()
+    return cookies
+
+
+def addIncome():
+    url = 'https://apibeta.yunchehome.com/newCost/bill/doAdd'
+    data = {
+        "billType": 1,
+        "data": "{\"startTime\":\"2018-10-01\",\"totalMoneyE2\":\"8000\",\"feeTypeId\":\"2375\",\"companyName\":\"北京百度网讯科技有限公司\",\"companyTypeId\":\"2441\",\"paymentTime\":\"2018-10-12\"}",
+        "routerName": "incomeTally"
+    }
+
+    res = requests.post(url, json=data, cookies=getCookies())
+    print(res.text)
+
+
+def main():
+    for i in range(101):
+        print(i)
+        addIncome()
+
+
+if __name__ == '__main__':
+    main()
